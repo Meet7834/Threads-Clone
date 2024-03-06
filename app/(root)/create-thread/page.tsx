3b3694599fd9aa86f22d95737628073a53d11b80
Meet <proobmeet7834@gmail.com>
2024-03-07 01:29:48 +0530

    restructured project

--- app/(root)/create-thread/page.tsx
+++
@@ -1,5 +0,0 @@
-async function Page(){
-    return <h1 className="head-text">Create a thread</h1>
-}
-
-export default Page;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { Flex, Image } from "@chakra-ui/react"
+
+const Header = () => {
+  return (
+    <Flex justifyContent={'center'} mt={6} mb={12}>
+        <Image />
+    </Flex>
+  )
+}
+
+export default Header
