9d03c919b01abe5217c11780403c8513a14147fb
Meet <proobmeet7834@gmail.com>
2024-03-09 12:01:12 +0530

    user page done

--- frontend/src/pages/UpdateProfilePage.jsx
+++ frontend/src/pages/UpdateProfilePage.jsx
@@ -26,12 +26,17 @@ export default function UpdateProfilePage() {
         password: "",
     });
     const fileRef = useRef(null);
+    const [updating, setUpdating] = useState(false);
 
     const showToast = useShowToast();
 
     const { handleImageChange, imgUrl } = usePreviewImg();
+
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        if (updating) return;
+        setUpdating(true);
 
         try {
             const res = await fetch(`/api/users/update/${user._id}`, {
@@ -41,16 +46,21 @@ export default function UpdateProfilePage() {
                 },
                 body: JSON.stringify({ ...inputs, profilePic: imgUrl }),
             });
+
             const data = await res.json(); // updated user object
             if (data.error) {
                 showToast("Error", data.error, "error");
                 return;
             }
+            
             showToast("Success", "Profile updated successfully", "success");
             setUser(data);
+            
             localStorage.setItem("user-threads", JSON.stringify(data));
         } catch (error) {
             showToast("Error", error, "error");
+        } finally {
+            setUpdating(false);
         }
     };
 
@@ -151,6 +161,7 @@ export default function UpdateProfilePage() {
                                 bg: "green.500",
                             }}
                             type='submit'
+                            isLoading={updating}
                         >
                             Submit
                         </Button>
--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -1,14 +1,42 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import UserHeader from "../components/UserHeader"
 import UserPost from "../components/UserPost"
+import useShowToast from "../hooks/useShowToast";
 
 const UserPage = () => {
+
+    const [user, setUser] = useState(null);
+    const { username } = useParams();
+    const showToast = useShowToast();
+
+    useEffect(() => {
+        const getUser = async () => {
+            try {
+                const res = await fetch(`/api/users/profile/${username}`);
+                const data = await res.json();
+                if (data.error) {
+                    showToast("Error", data.error, "error");
+                    return;
+                }
+                setUser(data);
+            } catch (error) {
+                showToast("Error", error, "error");
+            }
+        };
+
+        getUser();
+    }, [username, showToast]);
+
+    if (!user) return null;
+
     return (
         <>
-            <UserHeader />
+			<UserHeader user={user} />
             <UserPost likes={1300} replies={500} postImg="/post1.png" postTitle="Let's talk about threads." />
             <UserPost likes={451} replies={70} postImg="/post2.png" postTitle="Have a good day" />
             <UserPost likes={785} replies={257} postImg="/post3.png" postTitle="This is my first thread" />
-            <UserPost likes={2500} replies={950}  postTitle="Hello..." />
+            <UserPost likes={2500} replies={950} postTitle="Hello..." />
         </>
     )
 }
