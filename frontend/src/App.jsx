de334b90d9d5711a8c2abc8674bafb5a18614a45
Priyanshu <mehtapriyanshu408@gmail.com>
2024-03-10 22:44:00 +0530

    frontend for chat functionality added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Button, Container } from "@chakra-ui/react"
+import { Container, Box } from "@chakra-ui/react"
 import { Navigate, Routes, Route } from "react-router-dom"
 import UserPage from "./pages/UserPage"
 import PostPage from "./pages/PostPage"
@@ -9,36 +9,39 @@ import { useRecoilValue } from "recoil"
 import userAtom from "./atoms/userAtom"
 import UpdateProfilePage from "./pages/UpdateProfilePage"
 import CreatePost from "./components/CreatePost"
+import ChatPage from "./pages/ChatPage"
 
 function App() {
-
     const user = useRecoilValue(userAtom);
 
     return (
-        <Container maxW='620px'>
-            <Header />
-            <Routes>
-                <Route path='/' element={user ? <HomePage /> : <Navigate to='/auth' />} />
-                <Route path='/auth' element={!user ? <AuthPage /> : <Navigate to='/' />} />
-                <Route path='/update' element={user ? <UpdateProfilePage /> : <Navigate to='/auth' />} />
+        <Box position={"relative"} w='full'>
+            <Container maxW='620px'>
+                <Header />
+                <Routes>
+                    <Route path='/' element={user ? <HomePage /> : <Navigate to='/auth' />} />
+                    <Route path='/auth' element={!user ? <AuthPage /> : <Navigate to='/' />} />
+                    <Route path='/update' element={user ? <UpdateProfilePage /> : <Navigate to='/auth' />} />
 
-                <Route
-                    path='/:username'
-                    element={
-                        user ? (
-                            <>
-                                <UserPage />
-                                <CreatePost />
-                            </>
-                        ) : (
-                            <UserPage />
-                        )
-                    }
-                />
-                <Route path="/:username/post/:pid" element={<PostPage />} />
-            </Routes>
-        </Container>
-    )
-}
+                    <Route
+						path='/:username'
+						element={
+							user ? (
+								<>
+									<UserPage />
+									<CreatePost />
+								</>
+							) : (
+                                    <UserPage />
+                                )
+						}
+					/>
+					<Route path='/:username/post/:pid' element={<PostPage />} />
+					<Route path='/chat' element={user ? <ChatPage /> : <Navigate to={"/auth"} />} />
+				</Routes>
+			</Container>
+		</Box>
+    );
+};
 
-export default App
+export default App;
